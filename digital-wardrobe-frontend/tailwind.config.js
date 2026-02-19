868b4b6c21fd878b67990a12715b0f5553d1ae24
adrikasaxena <adrikasaxenaaa@gmail.com>
2026-02-19 20:10:33 +0530

    Add files via upload

---
+++ digital-wardrobe-frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        latte: "#f5efe6",
+        beige: "#e8dcca",
+        mocha: "#6f4e37",
+        cocoa: "#3e2723",
+      },
+    },
+  },
+  plugins: [],
+};
+
